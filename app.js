d9b17657f300a7028a8b8d300a66409e332b814a
Kevin Eddye <kevneddy@gmail.com>
2016-02-22 14:55:23 -0800

    Add a few starter routes, controller and views

---
+++ app.js
@@ -0,0 +1,16 @@
+var sentimeta = angular.module('sentimeta', ['ngRoute', '$locationProvider'])
+
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl : 'pages/home.html',
+		controller  : 'homeCtrl'
+	})
+	.when('/', {
+		templateUrl : '',
+		controller  : ''		
+	})
+
+	$locationProvider.html5Mode(true);
+})
---
+++ controllers/controller.js
@@ -0,0 +1,4 @@
+app.controller('homeCtrl', function($scope){
+	$scope.message = 'Hello World!'
+});
+
