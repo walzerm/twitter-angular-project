1ed30150a56cfd4ae77d194c837abb3966825da6
gregsim92 <gregsim92@gmail.com>
2016-02-22 15:57:34 -0800

    deleted unneccesary routes/users.js and db/migrations folder

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var knex = require('./db/knex');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 app = express();
 
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
