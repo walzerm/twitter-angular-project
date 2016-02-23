17d929fae44c7fcf101d115ae1af61cf2b3323b1
gregsim92 <gregsim92@gmail.com>
2016-02-23 11:06:04 -0800

    added post request for signup, not tested yet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+var locus = require('locus');
+var bcrypt = require('bcrypt');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { username: 'Sentimeta' });
 });
+
+router.post('/new', function(req,res,next){
+	knex('users').where({username:req.body.username}).first().then(function(user){
+		if (user || req.body.password !== req.body.passwordConfirm){
+			//#TODO: create error route/template
+			res.send('DANGER: USERNAME/PASSWORD ERROR');
+		} else { bcrypt.genSalt(10, function(err, salt){
+
+            bcrypt.hash(req.body.password, salt, function(err, hash){
+
+            knex('users').insert({username: req.body.username, password: hash}).returning('id').then(function(id){
+              //#TODO: create either a user template or just redirect home
+            });
+          });
+        });
+      }
+	})
+})
+
+router.get('/user', function(req,res,next){
+	
+})
+
 
 module.exports = router;
