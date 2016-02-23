f2e57d4be4545db4c76da73bea1a85dab070fc7e
Lissa Walzer <lissawalzer@gmail.com>
2016-02-23 14:40:42 -0800

    Put tweet data into object for processing

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -28,7 +28,18 @@ function getTweets(screenName) {
             "Authorization": "Bearer " + process.env.TWITTER_BEARER_TOKEN
         }
     }, function(err, res, body) {
-        console.log(body);
+        var tweets = [];
+
+        body.forEach(function(tweet) {
+            var tweet = {
+                text: tweet.text,
+                date: new Date(tweet.created_at),
+                retweets: tweet.retweet_count,
+                favs: tweet.favorite_count
+            };
+            tweets.push(tweet);
+        })
+        console.log(tweets);
     })
 }
 
