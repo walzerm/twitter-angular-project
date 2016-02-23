29a893b5838bf59d2808fc86407ae06f5caf6ef0
Lissa Walzer <lissawalzer@gmail.com>
2016-02-23 13:59:21 -0800

    Retrived bearer token from Twitter

---
+++ server/config/getBearerToken.js
@@ -0,0 +1,22 @@
+
+var request = require("request");
+
+var key = process.env.TWITTER_CONSUMER_KEY;
+var secret = process.env.TWITTER_CONSUMER_SECRET;
+var cat = key + ":" + secret;
+var credentials = new Buffer(cat).toString('base64');
+var url = 'https://api.twitter.com/oauth2/token';
+
+request({
+    url: url,
+    method: 'POST',
+    headers: {
+        "Authorization": "Basic " + credentials,
+        "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
+    },
+    body: "grant_type=client_credentials"
+
+}, function(err, resp, body) {
+    // Save this as the bearer token
+    console.log(body);
+});
