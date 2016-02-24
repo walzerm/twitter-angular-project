2bc80a07fa27859e7924d86e5d9c688ce9cbe6fa
LUIS ROCHA <luisrocha1993@gmail.com>
2016-02-24 10:54:40 -0800

    login modal

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
